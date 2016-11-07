92e666564b017a5aadf6343f9ac16cee6f25cc3c
codevonnie <yvonnegrealy@hotmail.com>
2016-11-07 17:22:15 +0000

    Updates
    - Removed Ionic 2 code
    - Add auth code to Ionic project

--- MobileApp/memberme/www/js/app.js
+++ MobileApp/memberme/www/js/app.js
@@ -5,20 +5,16 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.routes'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.routes', 'ngMockE2E'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
+    if (!AuthService.isAuthenticated()) {
+      console.log(next.name);
+      if (next.name !== 'outside.login' && next.name !== 'outside.register') {
+        event.preventDefault();
+        $state.go('outside.login');
+      }
     }
   });
 });
