b05a82221b688df5fd0f6326a516fb7e2efb2f62
codevonnie <yvonnegrealy@hotmail.com>
2017-01-25 16:14:18 +0000

    Update cancel now working
    - cancel and submit buttons correcting on page
    - started on geolocation for sos view
    - date joined and dob displaying as strings

--- MobileApp/memberme/www/js/controllers.js
+++ MobileApp/memberme/www/js/controllers.js
@@ -35,12 +35,19 @@ $scope.login = function(user) {
 .controller('ProfileCtrl', function($scope, AuthService, API_ENDPOINT, $http, $state, $ionicListDelegate) {
   
     $scope.profile={}; //empty profile object
+    $scope.origProfile={};
     $scope.toggle=false; 
     $scope.listCanSwipe = true; //profile list is swipeable
     var profileData=window.localStorage.getItem('profile'); //get profile data from local storage
     $scope.qrcode=profileData; //qrcode text is the object 
     profileData=JSON.parse(profileData); //parse JSON object
     $scope.profile=profileData; //save JSON object to $scope variable
+    $scope.origProfile=profileData;
+    var joined = new Date(parseInt(profileData.joined)).toDateString();
+    var dob = new Date(parseInt(profileData.dob)).toDateString();
+    console.log(joined);
+    $scope.joined = joined;
+    $scope.dob = dob;
 
     //edit function - triggered when user hits edit on profile items
     $scope.edit = function() {
@@ -49,18 +56,20 @@ $scope.login = function(user) {
     }
     //cancel method on profile edit
     $scope.cancel = function(){
+      
       $scope.toggle=!$scope.toggle; //hide input box
-      console.log($scope.profile.name);
+      profileData=window.localStorage.getItem('profile');
+      profileData=JSON.parse(profileData); //parse JSON object
+      $scope.profile=profileData; //save JSON object to $scope variable
     }
     //submit function for profile edits
     $scope.submit = function() {
       $scope.toggle=!$scope.toggle; //hide input box
       AuthService.updateProfile($scope.profile); //call AuthService method updateProfile and pass $scope.profile object
-      profileData=$scope.profile; //save $scope obj to profileData obj
+      profileData=$scope.profile; //save updated $scope obj to profileData obj
+      window.localStorage.setItem('profile', JSON.stringify(profileData)); //set updated profile details to local storage
       $scope.qrcode=JSON.stringify(profileData); //set qr text to updated profile details
     }
-  
-  //func when user is logging out
 
   //calls AuthService method getInfo to authorize user on db and retrieve profile details
   $scope.getInfo = function() {
@@ -83,8 +92,29 @@ $scope.login = function(user) {
 
 
 
-.controller('SosCtrl', function($scope, AuthService, $state) {
+.controller('SosCtrl', function($scope, AuthService, $state, $ionicLoading) {
   
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
  
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    });
 });//end SosCtrl
  
