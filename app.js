cb959eb3e771611138f15f25c97071b2aa1700bc
UnderDev <scottcoyne@hotmail.com>
2017-02-08 17:21:13 +0000

    Bug Fix Testing

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(sse);
 app.all('*', function (req, res, next) {
   // do logging
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
   res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
   console.log('\nSomeOne Connected');
 
