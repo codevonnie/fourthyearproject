34275910a58adb88d47d45789651001e88077f94
Yvonne Grealy <yvonnegrealy@hotmail.com>
2017-02-16 16:36:29 +0000

    Removed lastVisited from profile
    - Attribute no longer exists for Person object

--- MobileApp/memberme/www/js/controllers.js
+++ MobileApp/memberme/www/js/controllers.js
@@ -187,17 +187,14 @@ angular.module('starter.controllers', [])
     $scope.qrcode=$scope.profile.email; //qrcode text is the object 
     $scope.profile.icePhone = parseInt(profileData.icePhone); //convert phone numbers to strings
     $scope.profile.phone = parseInt(profileData.phone);
-     
+    console.log(profileData);
     //convert date milliseconds to date string - local variables so person object is not amended
     var joined = new Date(parseInt(profileData.joined)).toDateString();
     var dob = new Date(parseInt(profileData.dob)).toDateString();
     var membership = new Date(parseInt(profileData.membership)).toDateString();
-    var lastVisited = new Date(parseInt(profileData.lastVisited)).toDateString();
     
     $scope.joined = joined;
     $scope.dob = dob;
-    $scope.lastVisited = lastVisited;
-
     
     //if membership is not a valid date set it to null or save the date value to the scope
     if(membership=='Invalid Date' || profileData.membership=='0'){
